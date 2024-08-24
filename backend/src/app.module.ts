8d45f5e27f472376f502cc1e132a4846e807cd7e
Duvan Mancilla <Duvanred0@gmail.com>
2024-08-24 16:54:35 -0500

    remplazo de entities por nuestra entitie Table

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,21 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { table } from './entities/table.entity';
-//Creamos un modulo para conectar la base de datos en cuanto sus propiedades
+import { Table } from './entities/table.entity';
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type : 'postgres',
-      host : 'localhost',
+      type: 'postgres',
+      host: 'localhost',
       port: 5432,
       username: 'postgres',
       password: '1234',
       database: 'Market',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'], //facilita el encuentro de las entidades 
-      synchronize: true, 
-    })
-    TypeOrmModule.forFeature([table]), //ademas de conectarla, hacemos uso de las entidades donde se especifican los tipos de datos y la cantidad de datos
+      entities: [Table],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([Table]),
   ],
   controllers: [AppController],
   providers: [AppService],
