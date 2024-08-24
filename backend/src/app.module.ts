fee8bb4f5adb12bbe4bd11a25e1bf33b4afd6c6d
Duvan Mancilla <Duvanred0@gmail.com>
2024-08-24 16:05:21 -0500

    añadiendo documentacion app.module y su comunicacion con entidades

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,7 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
+import { table } from './entities/table.entity';
+//Creamos un modulo para conectar la base de datos en cuanto sus propiedades
 @Module({
   imports: [
     TypeOrmModule.forRoot({
@@ -12,9 +13,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       username: 'postgres',
       password: '1234',
       database: 'Market',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      entities: [__dirname + '/**/*.entity{.ts,.js}'], //facilita el encuentro de las entidades 
       synchronize: true, 
     })
+    TypeOrmModule.forFeature([table]), //ademas de conectarla, hacemos uso de las entidades donde se especifican los tipos de datos y la cantidad de datos
   ],
   controllers: [AppController],
   providers: [AppService],
