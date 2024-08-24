69e8ece108f5d0749b9c24defda5ddf4478959d6
Duvan Mancilla <Duvanred0@gmail.com>
2024-08-24 15:57:10 -0500

    añadiendo documentacion codigo entidades para base de datos PostgreSQL

--- backend/src/entities/table.entity.ts
+++ backend/src/entities/table.entity.ts
@@ -1,5 +1,7 @@
 import { Entity,Column, PrimaryGeneratedColumn } from "typeorm";
 
+//al igual que express necesitamos hacer referencia al tipo de dato y nombre del dato el cual estamos manejado en la DB
+//en este caso se llaman entidades, usamos typecript para decirle que dato deberia usar en caso que no encuentre el tipo
 @Entity()
 export class table{
     @PrimaryGeneratedColumn()
@@ -8,9 +10,10 @@ export class table{
     @Column()
     name: string
 
-    @Column()
+    @Column('float')
     price: number
 
     @Column()
     description: string
+    //estas son las columnas dentro de nuestra base de datos en PostgreSQL 
 }
