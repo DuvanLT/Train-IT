ccfd5b635521b59d6d5a095001a207709d7fb2a8
Duvan Mancilla <Duvanred0@gmail.com>
2024-08-24 15:54:49 -0500

    añadiendo documentacion codigo de servicios

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -9,8 +9,10 @@ export class ApiService {
   private apiUrl = 'http://localhost:3000';  // URL del backend
 
   constructor(private http: HttpClient) {}
-
+    //creamos un contrcutor haciendo referencia a manejo de protocolo http
   getData(): Observable<any> {
     return this.http.get(`${this.apiUrl}/products`);
+    //Para realizar un endpoint y poder acceder a la data, llamamos el URL del backend y le aumentamos el endpoint de esta manera
+    //tendremos conectado el back + base datos al front , http.get indica que estaremos eligiendo una ruta
   }
 }
